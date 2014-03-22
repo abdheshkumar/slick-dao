f5b62d6001aabc0bda147ce0ee8d51b74ac059f9
Jan Christopher Vogt <christopher.vogt@epfl.ch>
2014-03-22 16:53:27 +0100

    Remove mutability from code generator
    No need for mutability here

--- src/main/scala/scala/slick/dao/Generators.scala
+++ src/main/scala/scala/slick/dao/Generators.scala
@@ -26,8 +26,8 @@ object Generators {
       val mTable = MTable(MQName(None, Some(scheme), tableName), "TABLE", null, None, None, None)
       val model = createModel(Seq(mTable), DBConnection.profile)
       val codeGen = new DaoSourceGenerator(model)
-      val code = package(codeGen, packag, daoClass, updateFactory)
-      writeResult(s"$folder/$packag/${codeGen.entity}.scala", code)
+      val code = packageUp(codeGen, packag, daoClass, updateFactory)
+      writeResult(s"$folder/$packag/${codeGen.entityName}.scala", code)
     }
 
     private def writeResult(path: String, data: String) {
@@ -38,24 +38,29 @@ object Generators {
 
 
     def packageUp(generator: DaoSourceGenerator, packag: String, daoClass: String, genFactory: Boolean) = {
-      val factory = if(genFactory) val string = s"  def get$daoClass: I$daoClass = new $daoClass()" else ""
+      assert(generator.tables.size == 1)
+      val table = generator.tables.head
+      val (pkName,pkType) = table.columns.filter(_.model.options.contains(ColumnOption.PrimaryKey)).map{
+        col => (col.rawName,col.rawType.toString)
+      }.head
+      val factory = if(genFactory) s"  def get$daoClass: I$daoClass = new $daoClass()" else ""
       s"""package $packag
 import scala.slick.dao.DBConnection.profile.simple._
 import scala.slick.dao._
 ${generator.code}
 
-trait I$daoClass extends AbstractDAO with CRUDable[${generator.table}, ${generator.pkType}]
+trait I$daoClass extends AbstractDAO with CRUDable[${table.TableClass.name}, ${pkType}]
 
 class $daoClass(implicit innerSession: Session) extends I$daoClass {
 
-  val entities: TableQuery[${generator.table}] = TableQuery[${generator.table}]
+  val entities: TableQuery[${table.TableClass.name}] = TableQuery[${table.TableClass.name}]
 
-  def selectBy(entity: ${generator.entity}) = {
-    for (e <- entities if e.${generator.pkName} === entity.${generator.pkName}) yield e
+  def selectBy(entity: ${table.EntityType.name}) = {
+    for (e <- entities if e.${pkName} === entity.${pkName}) yield e
   }
 
-  def selectById(id: ${generator.pkType}) = {
-    for (e <- entities if e.${generator.pkName} === id) yield e
+  def selectById(id: ${pkType}) = {
+    for (e <- entities if e.${pkName} === id) yield e
   }
   $factory
 }"""
@@ -63,23 +68,10 @@ class $daoClass(implicit innerSession: Session) extends I$daoClass {
   }
 
   class DaoSourceGenerator(model: Model) extends SourceCodeGenerator(model) {
-    // need for DAO generation
-    var table: String = _
-    var entity: String = _
-    var pkName: String = _
-    var pkType: String = _
-
     // override mapped table and class name
-    override def entityName =
-      dbTableName => {
-        entity = dbTableName.toLowerCase.toCamelCase
-        entity
-      }
-    override def tableName =
-      dbTableName => {
-        table = dbTableName.toLowerCase.toCamelCase + "s"
-        table
-      }
+    override def entityName = _.toLowerCase.toCamelCase
+
+    override def tableName = _.toLowerCase.toCamelCase + "s"
 
     // remove ForeignKeyAction import
     override def code = super.code.substring(super.code.indexOf("\n"))
@@ -102,19 +94,6 @@ class $daoClass(implicit innerSession: Session) extends I$daoClass {
       override def TableClass = new TableClass {
         override def optionEnabled: Boolean = false
       }
-
-      // override contained column generator
-      override def Column = new Column(_) {
-        // use the data model member of this column to change the Scala type, e.g. to a custom enum or anything else
-        override def rawType = {
-          if (model.options.contains(ColumnOption.PrimaryKey)) {
-            pkName = super.rawName
-            pkType = super.rawType.toString
-          }
-          super.rawType
-        }
-      }
     }
   }
-
 }
